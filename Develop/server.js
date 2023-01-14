8ae78a572712366cdf837864a45a4eef09236499
Adam <asantillana@apple.com>
2023-01-14 13:30:46 -0600

    required db to retrieve past notes when calling /notes

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const app = express();
 const PORT = 3001;
+const notes = require('./db/db.json');
+
+// Middleware for parsing application/json
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -10,30 +14,27 @@ app.get('/notes', (req,res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-// GET request
+// GET request for Notes
 app.get('/api/notes', (req, res) => {
-    // Let the client know that their request was received
-    res.json(`${req.method} request received`);
-  
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-  
     // Log our request to the terminal
-    console.info(`${req.method} request received`);
+    console.info(`${req.method} request received to get notes`);
+  
+    // Sending all reviews to the client
+    return res.status(200).json(notes);
   });
 
   // POST request
-app.post('/api/reviews', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // Let the client know that their POST request was received
     res.json(`${req.method} request received`);
   
     // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
+    console.log(req.body);
   
     // Log our request to the terminal
-    console.info(`${req.method} request received`);
+    console.log(`${req.method} request received`);
   });
-  
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );

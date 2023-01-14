719dd6fd5d89eab33d4e01cb936a109d87744929
Adam <asantillana@apple.com>
2023-01-14 13:25:25 -0600

    Added GET and POST code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,30 @@ app.get('/notes', (req,res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// GET request
+app.get('/api/notes', (req, res) => {
+    // Let the client know that their request was received
+    res.json(`${req.method} request received`);
+  
+    // Show the user agent information in the terminal
+    console.info(req.rawHeaders);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received`);
+  });
 
+  // POST request
+app.post('/api/reviews', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received`);
+  
+    // Show the user agent information in the terminal
+    console.info(req.rawHeaders);
+  
+    // Log our request to the terminal
+    console.info(`${req.method} request received`);
+  });
+  
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );

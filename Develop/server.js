2ef2d00ddad177431e8823985dd90032926c4b44
adam42288 <a75052@gmail.com>
2023-01-15 15:13:09 -0600

    moved public above the other routes

--- Develop/server.js
+++ Develop/server.js
@@ -7,12 +7,13 @@ const app = express();
 const notes = require('./db/db.json');
 // Helper method for generating unique note ids
 const uuid = require('./helpers/uuid');
+app.use(express.static('public'));
 
 // Middleware for parsing application/json
 app.use(express.json());
 app.use('/', htmlrouter);
 app.use('/api', api);
 
-app.use(express.static('public'));
 
-app.listen(process.env.PORT || 5001);
+
+app.listen(process.env.PORT || 3001);
